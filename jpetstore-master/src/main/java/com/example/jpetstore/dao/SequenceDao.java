57e079669082b3e8911fca30d924da7c9242dd63
jpg723 <86431761+jpg723@users.noreply.github.com>
2023-05-21 16:41:35 +0900

    [delete] folder

--- jpetstore-master/src/main/java/com/example/jpetstore/dao/SequenceDao.java
+++
@@ -1,7 +0,0 @@
-package com.example.jpetstore.dao;
-
-import org.springframework.dao.DataAccessException;
-
-public interface SequenceDao {
-	public int getNextId(int id) throws DataAccessException;
-}
